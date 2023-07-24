998492920359771831abbcb09983b4fbcbfc59ce
Muhammad Umair Sadiq <75296180+MuhammadUmairSadiq@users.noreply.github.com>
2023-07-24 07:17:53 -0700

    Navbar

---
+++ src/component/utilis/NavArrayTypes.ts
@@ -0,0 +1,68 @@
+export interface NavbarItemType {
+    label: string,
+    href: string,
+    isDropDown: boolean,
+    dropDownData?: Array<NavbarItemType>,
+};
+ 
+
+export const NavbarArray:Array<NavbarItemType> = [
+    {
+        label : "Female",
+        href: "/female",
+        isDropDown:true,
+        dropDownData :     [{
+            label: "Dresses",
+            href: "/female/Dresse",
+            isDropDown: false,
+        },
+        {
+            label: "T-Shirts",
+            href: "/female/T-shirt",
+            isDropDown: false,
+        },
+        {
+            label: "Pents",
+            href: "/female/Pant",
+            isDropDown: false,
+        },
+        {
+            label: "Jackets",
+            href: "/female/Jacket",
+            isDropDown: false,
+        },
+        {
+            label: "Sweater",
+            href: "/female/Sweater",
+            isDropDown: false,
+        },
+    ]
+    },
+    {
+        label : "Male",
+        href: "/male",
+        isDropDown:true,
+        dropDownData: [
+            {
+                label: "Sweaters",
+                href: "/male/Sweater",
+                isDropDown: false,
+            },
+            {
+                label: "Jackets",
+                href: "/male/Jacket",
+                isDropDown: false,
+            },
+        ]
+    },
+    {
+        label : "Kids",
+        href: "/kids",
+        isDropDown:false,
+    },
+    {
+        label : "All Products",
+        href: "/allproducts",
+        isDropDown:false,
+    }
+]
---
+++ src/component/views/Navbar/index.tsx
@@ -0,0 +1,71 @@
+"use client"
+import { NavbarArray,NavbarItemType } from "@/component/utilis/NavArrayTypes"
+import {BiSearch} from 'react-icons/bi'
+import {HiOutlineChevronDown} from 'react-icons/hi'
+import { GiHamburgerMenu } from "react-icons/gi"
+import {BsFillCartCheckFill} from 'react-icons/bs'
+import { IoMdClose } from "react-icons/io"
+import Image from "next/image"
+import Link from "next/link"
+import DropDown from "../subcomponents/DropDown"
+import {useState} from "react"
+import Expand from '../subcomponents/Expand'
+
+const Navbar = () => {
+    const [isNavbarOpen, setNavbarOpen] = useState<boolean>(false);
+    const [cardItemNumber,isCardItemNumber] = useState<number>(0);
+    return(
+        <div>
+        <div className="py-6 flex items-center justify-between"  >
+            <div className="w-36 flex-shrink-0">
+                <Image src={"/logo.webp"} alt="Logo" width={500} height={500}></Image>
+            </div>
+           <div className="hidden lg:flex justify-between items-center space-x-12">
+           <ul className="flex space-x-4 font-medium text-lg text-purple-950">
+                {NavbarArray.map((item: NavbarItemType,index:number)=> (
+                     <li key={index} className="flex items-center relative rounded-sm px-3 py-1 hover:bg-gray-100 cursor-pointer group">
+                     <Link href={item.href} >{item.label}</Link>
+                     {item.isDropDown ? <HiOutlineChevronDown className="mt-1 -rotate-180 group-hover:rotate-0 duration-300" size={15} /> : ""}
+                     {item.isDropDown && <div className={`invisible group-hover:visible absolute top-8 left-0 py-2 px-6 bg-gray-100 font-light min-w-[7.8rem]`}>
+                         <DropDown item={item} />
+                     </div>}
+                 </li>
+                ))}
+            </ul>
+            <div className="border flex items-center text-gray-600 px-3 rounded-md">
+                <BiSearch/>
+                <input type="text" className="pl-1 pr-5 py-1 w-80 flex-grow" placeholder="What are you looking for?" />
+            </div> 
+            <div className="relative flex-shrink-0 w-11 h-11 flex bg-gray-300 rounded-full items-center justify-center">
+                <div className="absolute w-4 h-4 bg-red-400 top-1 text-xs rounded-full font-light
+                                flex items-center justify-center right-0">3</div>
+                <BsFillCartCheckFill size={25}/>
+            </div>
+           </div>
+           
+           <div onClick={()=> setNavbarOpen(!isNavbarOpen)}>
+                {isNavbarOpen ? <div className="flex lg:hidden"><IoMdClose size={25}/></div> :
+                <div className="flex lg:hidden"><GiHamburgerMenu size={25}/> </div> }
+            </div>
+        </div>
+            {
+                isNavbarOpen && <Mobilenav/>
+            }
+        </div>
+    )
+}
+
+export default Navbar
+
+const Mobilenav = () => {
+    const [isExpanded, setExpanded] = useState<boolean>(false);
+    return (
+        <div className="w-full px-6 py-4 bg-grey-100">
+            {
+                NavbarArray.map((item:NavbarItemType,index:number)=>(
+                    <Expand key={index} item = {item}/>
+                ))
+            }
+        </div>
+    )
+}

10252aff023b0de4e394a9d6a9cc689586b8699a
Muhammad Umair Sadiq <75296180+MuhammadUmairSadiq@users.noreply.github.com>
2023-07-25 06:48:28 -0700

    forth part data from sanity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
-import Main from '@/component/views/Main'
-import ProductType from '@/component/views/ProductType'
-import Image from 'next/image'
+import Main from "@/component/views/Main";
+import ProductType from "@/component/views/ProductType";
+import { oneProductType, responseType } from "@/component/utilis/ProductsType";
+import ProductCard from "@/component/views/ProductCard";
 
-export default function Home() {
+async function fetchAllProductsData() {
+  let res = await fetch(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "products"]`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  let { result }: responseType = await fetchAllProductsData();
   return (
     <div>
-      <Main/>
-      <ProductType/>
+      <Main />
+      <ProductType />
+      <ProductCard productData = {result}/>
     </div>
-  )
+  );
 }
---
+++ src/component/views/ProductCard/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+const ProductCard = ({ productData }:any) => {
+  return <div>
+    Product
+  </div>;
+};
+
+export default ProductCard

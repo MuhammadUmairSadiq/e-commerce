763215d96b4851559573754101c1dcd5b6725b2b
Muhammad Umair Sadiq <75296180+MuhammadUmairSadiq@users.noreply.github.com>
2023-08-01 22:53:10 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Wrapper>
-          
+          <Navbar />
+          {children}
+          <Footer />
         </Wrapper>
       </body>
     </html>

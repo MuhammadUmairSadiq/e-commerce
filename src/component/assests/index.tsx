7105281eecaa281e9d5e7aedbadbccbe1561b09d
Muhammad Umair Sadiq <75296180+MuhammadUmairSadiq@users.noreply.github.com>
2023-08-05 00:23:26 -0700

    Update index.tsx

--- src/component/assests/index.tsx
+++ src/component/assests/index.tsx
@@ -1,3 +1,2 @@
 export {default as mainImage} from './images/mainImage.webp'
 export { default as profile } from "./images/profile.jpeg";
-export { default as PreLoader } from "./images/Preloader.gif";
